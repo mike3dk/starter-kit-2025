c2a4c28dc080f8be27227453972ff3a58ed3d436
Lisa Sunhwa Han <ellesis@gmail.com>
2025-04-07 21:36:52 -0700

    feat: add next-intl

--- app/page.tsx
+++ app/page.tsx
@@ -3,23 +3,26 @@ import { prisma } from "@/lib/prisma"
 import AuthButtons from "@/components/auth-buttons"
 import { Button } from "@/components/ui/button"
 import type { Test } from "@prisma/client"
-import Link from "next/link"
+
+import { getTranslations } from "next-intl/server"
 
 export default async function Home() {
+  const t = await getTranslations()
   const tests = await prisma.test.findMany()
 
   return (
     <div className="flex h-screen flex-col items-center justify-center">
-      starter kit
+      {t("title")}
       <div className="mb-4">
         {tests.map((test: Test) => (
           <div key={test.id}>{test.name}</div>
         ))}
       </div>
-      <Button>Click me!</Button>
+      <Button>{t("click-me")}</Button>
       <div className="p-4">
         <AuthButtons />
       </div>
+      <div className="p-4"></div>
     </div>
   )
 }
