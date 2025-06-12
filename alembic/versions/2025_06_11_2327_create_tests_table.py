c14412625dae871940f93f23bed26bdfc8da27da
Julia Kang <julia7hk@gmail.com>
2025-06-11 23:29:03 -0700

    alembic revision to create tests table

---
+++ alembic/versions/2025_06_11_2327_create_tests_table.py
@@ -0,0 +1,26 @@
+"""create tests table
+
+Revision ID: f9ee08773633
+Revises: 
+Create Date: 2025-06-11 23:27:20.053024
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'f9ee08773633'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
