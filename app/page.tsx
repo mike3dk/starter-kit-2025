4ec873565f1c79c7481784464881bbeca77549c9
Julia Kang <julia7hk@gmail.com>
2025-06-20 23:09:18 -0700

    prisma setup for nextjs (#5)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
-export default function Home() {
+import { prisma } from '@/lib/prisma'
+import type { Test } from "@prisma/client"
+
+export default async function Home() {
+  const tests = await prisma.test.findMany();
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       starter kit
+
+      <div>
+        {tests.map((test: Test) => (
+          <div key={test.id}>{test.name}</div>
+        ))}
+      </div>
     </div>
   );
 }
