ecb335699f6df919766acd02dcce1a2d36a1f8cf
Michael Kang <michael3dk@gmail.com>
2025-06-26 21:07:19 -0700

    remove project specific information (#9)

--- alembic/versions/2000_01_01_0000_create_tests_table.py
+++ alembic/versions/2000_01_01_0000_create_tests_table.py
@@ -1,18 +1,19 @@
 """create tests table
 
-Revision ID: f9ee08773633
-Revises: 
-Create Date: 2025-06-11 23:27:20.053024
+Revision ID: 2000_01_01_0000
+Revises:
+Create Date: 2000-01-01 00:00:00.000000
 
 """
+
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = 'f9ee08773633'
+revision: str = "f9ee08773633"
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -27,7 +28,6 @@ def upgrade() -> None:
         )
         """
     )
-
 
 
 def downgrade() -> None:
