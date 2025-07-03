9b4d993a6fb78d021c56b10e2a7c68200c870a03
Julia Kang <julia7hk@gmail.com>
2025-07-02 22:04:35 -0700

    email test script

---
+++ app/api/email/route.ts
@@ -0,0 +1,20 @@
+import { sendEmail } from "@/lib/email"
+
+export async function POST(req: Request) {
+  return Response.json({ message: "Hello, world!" }, { status: 200 })
+
+  // the bottom code worked, blocking it for security reasons
+  const body = await req.json()
+  const { to, subject, message } = body
+
+  const result = await sendEmail({ to, subject, text: message, html: message })
+
+  if (result.error) {
+    return Response.json({ error: result.error }, { status: 500 })
+  }
+
+  return Response.json(
+    { message: "Email sent successfully", messageId: result.messageId },
+    { status: 200 }
+  )
+}
